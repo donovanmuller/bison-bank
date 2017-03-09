4ba80f8f8bd6ae852b53f2ed185a713f955c7f92
Donovan Muller <donovan.muller@gmail.com>
2017-03-09 03:02:36 +0200

    Added host with Thymeleaf

--- src/main/java/bison/Application.java
+++ src/main/java/bison/Application.java
@@ -1,10 +1,24 @@
 package bison;
 
+import static java.net.InetAddress.getLocalHost;
+
+import java.net.UnknownHostException;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
+@Controller
 public class Application {
+
+	@RequestMapping("/")
+	public String index(Model model) throws UnknownHostException {
+		model.addAttribute("host", getLocalHost().getHostName());
+		return "index";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
